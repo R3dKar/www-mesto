f8af02c912512350b6ad3a92989ba32ea35b8ad1
R3dKar <aazz.140720045@gmail.com>
2024-12-17 20:52:49 +0300

    feat: добавить карточки при загрузке

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,13 @@
 const cardTemplate = document.querySelector('#card-template').content;
+const cardsContainer = document.querySelector('.places__list');
 
 const createCard = card => {
-    const cardElement = cardTemplate.cloneNode();
+  const cardElement = cardTemplate.cloneNode();
 
-    cardElement.querySelector('.card__image').src = card.link;
-    cardElement.querySelector('.card__title').textContent = card.name;
+  cardElement.querySelector('.card__image').src = card.link;
+  cardElement.querySelector('.card__title').textContent = card.name;
 
-    return cardElement;
+  return cardElement;
 };
 
+initialCards.forEach(card => cardsContainer.append(createCard(card)));

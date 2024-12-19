47d1c6c9ef518da6689816cf448488b7ab9a755c
R3dKar <aazz.140720045@gmail.com>
2024-12-19 19:48:50 +0300

    refactor: исправить связь файлов в webpack

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,6 @@
+import '../pages/index.css';
+import { initialCards } from './cards.js';
+
 const cardTemplate = document.querySelector('#card-template').content;
 const cardsContainer = document.querySelector('.places__list');
 const nameElement = document.querySelector('.profile__title');

4d3d9a40d4c9be292e05812cc27897b41bfa9144
R3dKar <aazz.140720045@gmail.com>
2024-12-20 15:18:05 +0300

    feat: добавить закрытие окон по Esc и по клику в пустое место

--- src/scripts/modal.js
+++ src/scripts/modal.js
@@ -6,5 +6,16 @@ export const handleModalClose = event => closeModal(event.target.closest('.popup
 
 export const configureModal = popup => {
   popup.classList.add('popup_is-animated');
-  popup.querySelector('.popup__close').addEventListener('click', handleModalClose);
+
+  popup.querySelector('.popup__close').addEventListener('click', () => closeModal(popup));
+
+  popup.addEventListener('click', event => {
+    if (!event.target.classList.contains('popup')) return;
+    closeModal(popup);
+  });
+
+  document.addEventListener('keydown', event => {
+    if (event.key !== 'Escape') return;
+    closeModal(popup);
+  });
 };
